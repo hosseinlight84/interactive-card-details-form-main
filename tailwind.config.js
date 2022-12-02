55a1d4cad2219b4973feacbf5469bee1fbd54a30
hossein haji alizadeh <hosseinlight84@gmail.com>
2022-12-03 01:52:40 +0330

    completed side image and some form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/index.html'],
   theme: {
-    extend: {},
+    
+
+
+    extend: {
+      height: {
+        '48.5': '48.5rem',
+      },
+      spacing: {
+        '22': '88px',
+        '44.5': '163px',
+        '66': '260px',
+        '76': '308px',
+        '110': '455px',
+        '140': '474px',
+        '150': '536px',
+        '200': '900px',
+        
+      },
+},
   },
   plugins: [],
 }
