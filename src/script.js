5c4c9948d0ce2cac2725d60bf8f39d40c6376a64
hossein haji alizadeh <hosseinlight84@gmail.com>
2022-12-06 01:11:32 +0330

    completed responsive for mobile

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const cardnum = document.getElementById('crd-num');
+// const cardname = document.getElementById('card-name');
+// const date1 = document.getElementById('date1');
+// const date2 = document.getElementById('date2');
+// const cvc = document.getElementById('cvc');
+// const err = document.getElementById('err-txt');
+// const submit = document.getElementById('submit');
+// const frm1 = document.getElementById('frm1');
+
+
+
+
+  
+
+
